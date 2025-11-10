e42ca15841da449c982e979e3b9310fabfdaea2f
Vanson Wing Leung M2 16 <vansonhk@gmail.com>
2025-11-10 23:54:05 +0800

    host

--- vite.config.js
+++ vite.config.js
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => {
       },
     },
     server: {
+      host: env.VITE_HOST ? env.VITE_HOST : '0.0.0.0',
       port: env.VITE_PORT ? parseInt(env.VITE_PORT) : 5173,
     },
   }
